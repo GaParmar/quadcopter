5f0e8ee88b53eb6bf7849862c487f9609e8c0408
GaParmar <gparmar@ucsd.edu>
2017-05-03 00:29:59 -0700

    Create IMU.h

---
+++ IMU.h
@@ -0,0 +1,11 @@
+/*
+ * Description: Initializes the I2C communication with the
+ *              gyroscope
+ */
+void setupGyro();
+
+/*
+ * Description: Initializes the I2C communication with the
+ *              accelerometer
+ */
+void setupAccel();
