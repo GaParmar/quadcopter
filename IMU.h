fb6d28bb3e368c948e2df2bd38860e39592e3b04
GaParmar <gparmar@ucsd.edu>
2017-05-03 00:35:28 -0700

    Update IMU.h

--- IMU.h
+++ IMU.h
@@ -1,11 +1,23 @@
 /*
  * Description: Initializes the I2C communication with the
  *              gyroscope
+ * Parameters: none
+ * Return: none
  */
 void setupGyro();
 
 /*
  * Description: Initializes the I2C communication with the
  *              accelerometer
+ * Parameters: none
+ * Return: none
  */
 void setupAccel();
+
+/*
+ * Description: Calculates the gyroscope offests
+ * Parameters: int samples - the number of samples to be taken
+ * Return: An array of double with calculated gyroscope offsets
+ *        for each axis.
+ */
+double[] getGyroOffsets(int samples);
