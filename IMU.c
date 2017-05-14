54036d65c78273ddbb3e510a229e8d778e492c72
Gaurav Parmar <gparmar@ucsd.edu>
2017-05-13 23:49:34 -0700

    Create IMU.c

---
+++ IMU.c
@@ -0,0 +1,52 @@
+/*
+ * Description: Initializes the I2C communication with the
+ *              gyroscope
+ * Parameters: none
+ * Return: none
+ */
+void setupGyro()
+{
+  Wire.beginTransmission(105);
+  Wire.write(0x20);
+  Wire.write(0F);
+  Wire.endTransmission();
+  
+  Wire.beginTransmission(105);
+  Wire.write(0x23);
+  Wire.write(0x90);
+  Wire.endTransmission();
+}
+
+/*
+ * Description: Initializes the I2C communication with the
+ *              accelerometer
+ * Parameters: none
+ * Return: none
+ */
+void setupAccel()
+{
+  
+}
+
+/*
+ * Description: Calculates the gyroscope offests
+ * Parameters: int samples - the number of samples to be taken
+ * Return: An array of double with calculated gyroscope offsets
+ *        for each axis.
+ */
+double[] getGyroOffsets(int samples);
+
+/*
+ * Description: Updates the array with the latest gyroscope
+ *              readings. 
+ * Parameters: float * gr[] - pointer to float array
+ * Return: none
+ */
+void updateRawGyro(float * gr[]);
+
+/* Description: Updates the array with the latest accelerometer
+ *              readings. 
+ * Parameters: float * ac[] - pointer to float array
+ * Return: none
+ */
+void updateRawAccel(float * ac[]);
